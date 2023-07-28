0f42d60c679ef38714557d41192b6c354e6977dc
nicolas_nagano <nicolasjap@hotmail.com>
2023-07-28 02:00:19 -0300

    aula_31

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  // template: '<app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit{
+
+  allowServer = false;
+  serverCreationStatus = 'No Server was created!';
+  serverName = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowServer = true
+    },2000)
+  }
+ngOnInit(){
+}
+
+onCreateServer() {
+  this.serverCreationStatus = 'Server was created!';
+}
+
+onUpdateServerName(event: any) {
+  // console.log(event);
+  this.serverName = event.target.value
+}
 
 }
